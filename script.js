750f5c660b4efc1fb853141a91dfcf358e13452c
Daybringer <vanata.michal@gmail.com>
2020-03-07 20:58:13 +0100

    repaired one side of diagonal check

--- script.js
+++ script.js
@@ -21,10 +21,12 @@ let settings = {
 const tempSettings = { ...settings };
 
 let gamePlan = gamePlanGen(+(settings.gamePlan));
+let gamePlanRDiagonal = diagGamePlanGen(+(settings.gamePlan));
+let gamePlanLDiagonal = diagGamePlanGen(+(settings.gamePlan));
 htmlSquareGen(3);
 
 // Default state of 
-options_click("left_b", "right_b");
+options_click("right_b", "left_b");
 
 // generating array of game tiles
 function gamePlanGen(sizeOfGame) {
@@ -37,7 +39,27 @@ function gamePlanGen(sizeOfGame) {
   }
   return gameArray;
 }
+// generetaing diagonal gamePlan ==> unlike normal not equal arrays
+function diagGamePlanGen(sizeOfGame) {
+  let tempArr = [];
+  for (let x = 0; x < (sizeOfGame * 2 - 1); x++) {
+    tempArr.push([])
+  }
+  for (let x = 0; x < sizeOfGame; x++) {
+    for (let y = 0; y < x + 1; y++) {
+      tempArr[x].push(0);
+    }
+  }
 
+  let temp = sizeOfGame;
+  for (let x = sizeOfGame - 1; x > 0; x--) {
+    for (let y = 0; y < x; y++) {
+      tempArr[temp].push(0);
+    }
+    temp++;
+  }
+  return tempArr;
+}
 // Dynamicaly generating html divs for game plan
 function htmlSquareGen(sizeOfGame) {
   // removing old square grid
@@ -50,7 +72,7 @@ function htmlSquareGen(sizeOfGame) {
     for (let y = 0; y < sizeOfGame; y++) {
       let newSquare = document.createElement("div");
       newSquare.className = "square";
-      newSquare.id = `${x}${y}`;
+      newSquare.id = `${x}:${y}`;
       newSquare.onclick = () => game_click(newSquare.id);
       newSquare.style.gridArea = `${x + 1}/${y + 1}`;
       document.getElementsByClassName("game")[0].appendChild(newSquare);
@@ -65,8 +87,18 @@ function game_click(id) {
 
     document.getElementById(id).style.backgroundColor = (round % 2 === 1) ? settings.mainColor : settings.secondColor;
     document.getElementById(id).style.cursor = "default";
-    gamePlan[Number(id.charAt(0))][Number(id.charAt(1))] = (round % 2 === 1) ? "X" : "O";
+    let xGrid = Number(id.split(":")[0]);
+    let yGrid = Number(id.split(":")[1])
+    gamePlan[xGrid][yGrid] = (round % 2 === 1) ? "X" : "O";
+
+    let xPosition = 0;
+    let yPosition = (xGrid + yGrid < settings.gamePlan) ? yGrid : settings.gamePlan - (xGrid + yGrid) + yGrid - 1;
+
+    gamePlanLDiagonal[xGrid + yGrid][yPosition] = (round % 2 === 1) ? "X" : "O";
+    // gamePlanLDiagonal[Number(id.split(":")[0]) + Number(id.split(":")[1])][xPosition] = (round % 2 === 1) ? "X" : "O";
     round++;
+    console.log(gamePlanLDiagonal);
+    console.log(yPosition);
     win_check();
   } else {
 
@@ -86,14 +118,19 @@ function win_check() {
     var oCounterDR = 0;
     var xCounterDL = 0;
     var oCounterDL = 0;
+    var xCounterBDL = 0;
+    var oCounterBDL = 0;
+
 
     for (let y = 0; y < gamePlan[x].length; y++) {
 
       // Horizotal check + Empty squares check
       if (gamePlan[x][y] === "X") {
+        oCounterH = 0;
         xCounterH++;
       } else if (gamePlan[x][y] === "O") {
         oCounterH++;
+        xCounterH = 0;
       }
       if (gamePlan[x][y] === 0) {
         whiteCounter++;
@@ -102,42 +139,72 @@ function win_check() {
       // Vertical check
       if (gamePlan[y][x] === "X") {
         xCounterV++;
+        oCounterV = 0;
       } else if (gamePlan[y][x] === "O") {
         oCounterV++;
-      }
-
-      if ((x + y) < gamePlan.length) {
-        // Right diagonal check
-        if (y <= gamePlan[x + y].length) {
-
-          if (gamePlan[x + y][y] === "O") {
-            oCounterDR++;
-          } else if (gamePlan[x + y][y] === "X") {
-            xCounterDR++;
-          }
-        }
-
-        // Left diagonal check
-        if (y <= gamePlan[x + y].length) {
-          if (gamePlan[x + y][gamePlan[x + y].length - 1 - y] === "O") {
-            oCounterDL++;
-          } else if (gamePlan[x + y][gamePlan[x + y].length - 1 - y] === "X") {
-            xCounterDL++;
-          }
-        }
-
+        xCounterV = 0;
       }
 
       // Win condition check
-      if (xCounterV >= 3 || xCounterH >= 3 || xCounterDR >= 3 || xCounterDL >= 3) {
+      if (gamePlan.length === 3) {
+        if (xCounterV >= 3 || xCounterH >= 3) {
+          won("playerOne");
+          return true;
+        } else if (oCounterV >= 3 || oCounterH >= 3) {
+          won("playerTwo");
+          return true;
+        }
+      } else if (gamePlan.length === 15) {
+        if (xCounterV >= 5 || xCounterH >= 5) {
+          won("playerOne");
+          return true;
+        } else if (oCounterV >= 5 || oCounterH >= 5) {
+          won("playerTwo");
+          return true;
+        }
+      }
+    }
+  }
+
+  // Diagonal check
+  for (let x = 0; x < settings.gamePlan; x++) {
+    for (let y = 0; y < x + 1; y++) {
+
+      if (gamePlanLDiagonal[x][y] === "X") {
+        xCounterDL++;
+        oCounterDL = 0;
+      } else if (gamePlanLDiagonal[x][y] === "O") {
+        oCounterDL++;
+        xCounterDL = 0;
+      }
+
+      if (gamePlanLDiagonal[gamePlanLDiagonal.length - x - 1][y] === "X") {
+        xCounterBDL++;
+        oCounterBDL = 0;
+      } else if (gamePlanLDiagonal[gamePlanLDiagonal.length - x - 1][y] === "O") {
+        oCounterBDL++;
+        xCounterBDL = 0;
+      }
+    }
+    if (gamePlan.length === 3) {
+      if (xCounterDR >= 3 || xCounterDL >= 3 || xCounterBDL >= 3) {
         won("playerOne");
         return true;
-      } else if (oCounterV >= 3 || oCounterH >= 3 || oCounterDR >= 3 || oCounterDL >= 3) {
+      } else if (oCounterDR >= 3 || oCounterDL >= 3 || oCounterBDL >= 3) {
+        won("playerTwo");
+        return true;
+      }
+    } else if (gamePlan.length === 15) {
+      if (xCounterDR >= 5 || xCounterDL >= 5 || xCounterBDL >= 5) {
+        won("playerOne");
+        return true;
+      } else if (oCounterDR >= 5 || oCounterDL >= 5 || oCounterBDL >= 5) {
         won("playerTwo");
         return true;
       }
     }
   }
+
   if (whiteCounter === 0) {
     won("tie");
   }
@@ -159,7 +226,7 @@ function win_check() {
 
     let resetBtn = document.createElement("div");
     resetBtn.id = "resetBtn";
-    resetBtn.classList.add((player === "tie") ? "resetBtnP" : (player === "playerOne") ? ((settings.mainColor === "#ff1053") ? "resetBtnP" : "resetBtnB") : "resetBtnY");
+    resetBtn.classList.add((player === "tie") ? "resetBtnT" : (player === "playerOne") ? ((settings.mainColor === "#ff1053") ? "resetBtnP" : "resetBtnB") : "resetBtnY");
     resetBtn.innerHTML = "RESET";
     resetBtn.onclick = restart;
     document.getElementsByClassName("game_container")[0].appendChild(resetBtn);
@@ -172,8 +239,6 @@ function win_check() {
     winText.style.textShadow = `6px 4px 0px ${playerColors.grey}`;
     document.getElementsByClassName("game_container")[0].appendChild(winText);
   }
-
-
 }
 
 
@@ -210,11 +275,11 @@ function gameSizeClick(gameSizeInt) {
   if (gameSizeInt === 3) {
     document.getElementById("3x3").style.boxShadow = "inset 0px 0px 0px 10px #ff1053";
     document.getElementById("15x15").style.boxShadow = "none";
-    tempSettings.gamePlan = 3;
+    tempSettings.gamePlan = "3";
   } else if (gameSizeInt === 15) {
     document.getElementById("15x15").style.boxShadow = "inset 0px 0px 0px 10px #ff1053";
     document.getElementById("3x3").style.boxShadow = "none";
-    tempSettings.gamePlan = 15;
+    tempSettings.gamePlan = "15";
   } else {
     throw "unknown game size";
   }
@@ -229,6 +294,8 @@ function optionsFetch() {
 function restart() {
   htmlSquareGen(settings.gamePlan);
   gamePlan = gamePlanGen(+(settings.gamePlan));
+  gamePlanLDiagonal = diagGamePlanGen(+(settings.gamePlan));
+  gamePlanRDiagonal = diagGamePlanGen(+(settings.gamePlan));
   win = false;
   round = 1;
   if (document.getElementById("coverup")) {
@@ -240,11 +307,11 @@ function restart() {
   if (document.getElementById("resetBtn")) {
     document.getElementsByClassName("game_container")[0].removeChild(document.getElementById("resetBtn"));
   }
-  if (settings.gamePlan === 15) {
+  if (settings.gamePlan === "15") {
     Array.from(document.getElementsByClassName("square")).forEach((el) => {
       el.style.borderWidth = "1px";
     })
-  } else if (settings.gamePlan === 3) {
+  } else if (settings.gamePlan === "3") {
     Array.from(document.getElementsByClassName("square")).forEach((el) => {
       el.style.borderWidth = "3px";
     })
